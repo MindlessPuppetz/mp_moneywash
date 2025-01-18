72c34d3d2e3f08d893515599ca25c84542b0a3c4
MindlessPuppetz <mindlesspuppetz@gmail.com>
2025-01-18 14:45:29 -0900

    fix: replace ox_inv client call with proper server call and added debug

--- server/server.lua
+++ server/server.lua
@@ -7,22 +7,33 @@ AddEventHandler('mp_moneywash:withdraw', function(input, tax)
 	local markedAmount = 0
 
     if Config.Inventory  == 'ox' then
-        local items = exports.ox_inventory:Search('count', Config.BlackMoneyItem)
+        local items = exports.ox_inventory:GetItemCount(src, Config.BlackMoneyItem)
         markedAmount = items or 0
+        if Config.Debug then
+            print ("Step 1: markedAmount: ".. markedAmount)
+            print ("Step 1: items: ".. items)
+            print ("Step 1: input: ".. input)
+        end
     elseif Config.Inventory == 'qb' then
-        local items = PlayerData.items
+        local items = Player.PlayerData.items
         for k,v in pairs(items) do
             if v.name == Config.BlackMoneyItem then markedAmount = markedAmount + v.amount end
         end
     end
 
-    if (markedAmount >= input) and input >= 100 then
-        Player.Functions.RemoveItem(Config.BlackMoneyItem, input)
+    if (markedAmount >= input) and input >= Config.Minimum then
+        if Config.Debug then
+            print ("Step 2: markedAmount: ".. markedAmount)
+            print ("Step 2: input: ".. input)
+            print ("Step 2: tax: ".. tax)
+        end
         local newAmount = input - (input * tax)
         if Config.Inventory == "qb" then
+            Player.Functions.RemoveItem(Config.BlackMoneyItem, input)
             Player.Functions.AddMoney("cash", newAmount, "Money-Wash")
         elseif Config.Inventory == "ox" then
-            Player.Functions.AddItem(Config.MoneyItem, newAmount)
+            exports.ox_inventory:RemoveItem(src, Config.BlackMoneyItem, input)
+            exports.ox_inventory:AddItem(src, Config.MoneyItem, newAmount)
         end
         Core.Functions.Notify(src, "You received $".. newAmount .." in clean laundry." , "success")
         local creator = Player.PlayerData.charinfo.firstname..' '..Player.PlayerData.charinfo.lastname .."("..Player.PlayerData.source..")["..Player.PlayerData.citizenid.."]"
